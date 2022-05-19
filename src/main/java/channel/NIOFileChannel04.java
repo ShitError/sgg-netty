2a9afe50503ab67552f7179f7ec5238f991211b0
darkchen <1938976625@qq.com>
2022-05-19 16:32:29 +0800

    version 1.0

---
+++ src/main/java/channel/NIOFileChannel04.java
@@ -0,0 +1,28 @@
+package channel;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+
+public class NIOFileChannel04 {
+
+    public static void main(String[] args) throws Exception {
+
+        //创建相关流
+        FileInputStream fileInputStream = new FileInputStream("d:\\channelFile\\01.jpg");
+        FileOutputStream fileOutputStream = new FileOutputStream("d:\\channelFile\\02.jpg");
+
+        //获取各个流对应的 FileChannel
+        FileChannel sourceCh = fileInputStream.getChannel();
+        FileChannel destCh = fileOutputStream.getChannel();
+
+        //使用 transferForm 完成拷贝
+        destCh.transferFrom(sourceCh, 0, sourceCh.size());
+
+        //关闭相关通道和流
+        sourceCh.close();
+        destCh.close();
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+}
---
+++ src/main/readme.md
@@ -0,0 +1,1 @@
+https://blog.csdn.net/Youth_lql/article/details/115524052
